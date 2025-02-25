a9826691a4eae36ca412e4780cdf748bbb67bb33
Juan C. Jimenez Falcetta <140984823+Nheonz@users.noreply.github.com>
2025-02-25 05:19:01 +0100

    UpdateV0.5

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Toggle icon navbar
+// toggle icon navbar
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
@@ -6,40 +6,36 @@ menuIcon.onclick = () => {
   menuIcon.classList.toggle("bx-x");
   navbar.classList.toggle("active");
 };
-
-// Scroll sections
+// scroll sections
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-  let top = window.scrollY;
-
   sections.forEach((sec) => {
+    let top = window.scrollY;
     let offset = sec.offsetTop - 100;
     let height = sec.offsetHeight;
     let id = sec.getAttribute("id");
 
     if (top >= offset && top < offset + height) {
-      navLinks.forEach((link) => {
-        link.classList.remove("active"); // Remueve la clase de todos
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document;
+        document
+          .querySelector(`header nav a[href*="${id}"]`)
+          .classList.add("active");
       });
-
-      let activeLink = document.querySelector(`header nav a[href*="${id}"]`);
-      if (activeLink) {
-        activeLink.classList.add("active"); // Solo agrega a la sección actual
-      }
+      sec.classList.add("show-animate");
+    } else {
+      sec.classList.remove("show-animate");
     }
   });
-
-  // Sticky header
+  // sticky header
   let header = document.querySelector("header");
-  header.classList.toggle("sticky", window.scrollY > 100);
 
-  // Ocultar el menú en móviles al hacer clic en un enlace
-  navLinks.forEach((link) => {
-    link.addEventListener("click", () => {
-      menuIcon.classList.remove("bx-x");
-      navbar.classList.remove("active");
-    });
-  });
+  header.classList.toggle("sticky", window.scrollY > 100);
+  // remove toggle icon and navbar when click navbar links (scroll)
+  // animation footer on scroll
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
